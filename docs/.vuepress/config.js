604ffb6aeac9084dc3a4919494c444179e0898ac
LP0717 <LP0717@ldtt-ele.com>
2026-07-24 14:20:52 +0800

    feat: add Baidu Analytics tracking (ID: 896ed0566f9fdd9364a533ce8cec952a)

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,17 @@
 module.exports = {
   base: '/my-game-guide/',
   title: '热门游戏攻略站',
+  head: [
+    ['script', {}, `
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?896ed0566f9fdd9364a533ce8cec952a";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+      })();
+    `]
+  ],
   themeConfig: {
     nav: [
       { text: '首页', link: '/' },
